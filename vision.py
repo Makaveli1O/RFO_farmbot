ba8872b47f7c843766d76e5a7ef2704fbd01d2ee
Makaveli1O <samuel.liska49@gmail.com>
2023-01-25 13:23:56 +0100

    add: multiple overlapping objects detection

--- vision.py
+++ vision.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def findClickPositions(needle_img_path,
                        heystack_img,
-                       threshold = 0.5,
+                       threshold = 0.8,
                        debug_mode = None):
     """Finds sub image within image given heystack image. Uses TM_COEFF_NORMED
     algorithm to recieve number btwn 0 and 1. 1 indicading closest matched pixels.
@@ -21,25 +21,28 @@ def findClickPositions(needle_img_path,
     # searching img
     needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)  
     
+    # returns coefficients for every row/cols on every position even hanging over
     result = cv.matchTemplate(heystack_img, needle_img, cv.TM_CCOEFF_NORMED)
     
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    # get all locations over the threshold and store it into np array
+    locations = np.where(result >= threshold)
+    # merge indeces on to tuples (0,0), (1,1)
+    locations = list(zip(*locations[::-1]))
     
-    # found image within threshold
-    if max_val >= threshold:
-        # needle dimensions
+    if locations:
         needle_w = needle_img.shape[1]
         needle_h = needle_img.shape[0]
+        line_color = (0, 255, 0)
+        line_type = cv.LINE_4
         
-        top_left = max_loc
-        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        for location in locations:
+            # determine box around obj and draw box
+            top_left = location
+            bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+            cv.rectangle(heystack_img, top_left, bottom_right, line_color,1, line_type)
         
-        # draw rectangle around found obj
-        cv.rectangle(heystack_img, top_left, bottom_right,
-                     color = (0,255,0), thickness = 1, lineType = cv.LINE_4)
-        cv.imshow("Result", heystack_img)
-        cv.waitKey()
-    else:
-        print("Needle not found")
+    cv.imshow("Matcher", heystack_img)
+    cv.waitKey()
     
-findClickPositions("needle.jpg","heystack.jpg")
+findClickPositions("needle2.jpg","heystack2.jpg")
